a42ef4305a774252b4280662bda302e80de143f7
Aleks <example@gmail.com>
2020-08-13 20:50:41 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// alert(4 + +"5");
+
+// let incr=10,
+//     decr=10;
+
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+
+const ischek=false,
+      isClose=false;
+
+console.log(ischek || !isClose);
+
+
+    
